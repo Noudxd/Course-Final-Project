cda3ce9471b4d8f083e777bce3be5ef06cd2dbea
Noudxd <nadia.am@libero.it>
2023-02-12 19:31:18 +0100

    first commitù

---
+++ ritualbe/src/main/java/com/digitazon/ritualbe/RitualbeApplication.java
@@ -0,0 +1,13 @@
+package com.digitazon.ritualbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitualbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitualbeApplication.class, args);
+	}
+
+}
---
+++ ritualbe/src/test/java/com/digitazon/ritualbe/RitualbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitazon.ritualbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitualbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
